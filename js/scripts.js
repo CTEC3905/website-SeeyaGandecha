fbb3c605bb93bd075769064f426d2ef0ad601939
Seeya Gandecha <seeya@gandecha.net>
2018-12-30 13:23:51 +0000

    trial/error for image upload from directory with js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
 "use strict";
 
 console.log("hello");
+
+// var container = document.getElementById("wrapper");
+// var urls = ["imgs/TTTgalleries/amsterdam/" + ".jpg"];
+// for( i=0; i<urls.length; i++){
+//   container.insertAdjacentHTML('beforeend', '<img src="'+urls[i]+'">');
+// }
+
+
+
+// function test(){
+
+// var dir = "imgs/TTTgalleries/test/"; // folder location
+// var fileextension = ".jpg"; // image format
+// var i = "1";
+// var
+// $("<img />").attr('src', dir + i + fileextension ).appendTo(".test");
+
+//  if (i==10){
+//  alert('loaded');
+//  }
+//  else{
+//  i++;
+//  imageloop();
+// };
+// };   
+
+
+var container = document.getElementById("test");
+var dir = ["imgs/TTTgalleries/test/"];
+var fileextension = ".jpg";
+var i = 1
+
+for(i=0; i<dir.length; i++){
+  container.insertAdjacentHTML('beforeend', '<img src="'+dir[i]fileextension'">');
+if (i==10){alert('loaded') }
+else{i++;}
+
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 <!-- 
 instrgram widget from: https://instawidget.net/generate
-
+https://www.w3schools.com/howto/howto_css_image_overlay.asp
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_image_overlay_opacity 
  -->
